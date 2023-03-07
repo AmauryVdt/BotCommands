55de8da08c68309f955be295b6948c8798547d5e
AmauryVdt <amaury.vdt@gmail.com>
2023-03-07 23:50:26 +0100

    :sparkles: FeedBack command working properly!

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,33 +6,55 @@ module.exports = {
 		.setDescription('Send your feedback or bug report.'),
 	async execute(interaction) {
 		const modal = new ModalBuilder()
-			.setCustomId('myModal')
-			.setTitle('TSS Tournament');
+			.setCustomId('modalFeedback')
+			.setTitle('TSS Bot Feedback');
 
 		// Create the text input components
-		const favoriteColorInput = new TextInputBuilder()
-			.setCustomId('favoriteColorInput')
+		const typeFeedbackInput = new TextInputBuilder()
+			.setCustomId('typeFeedbackInput')
 			// The label is the prompt the user sees for this input
-			.setLabel("What's your favorite color?")
+			.setLabel('What is the type of the feedback ?')
 			// Short means only a single line of text
-			.setStyle(TextInputStyle.Short);
+			.setStyle(TextInputStyle.Short)
+			.setPlaceholder('Feedback, Bug Report, etc')
+			.setRequired(true);
 
-		const hobbiesInput = new TextInputBuilder()
-			.setCustomId('hobbiesInput')
-			.setLabel("What's some of your favorite hobbies?")
+		const contentFeedbackInput = new TextInputBuilder()
+			.setCustomId('contentFeedbackInput')
+			.setLabel('Your feedback :')
 			// Paragraph means multiple lines of text.
-			.setStyle(TextInputStyle.Paragraph);
+			.setStyle(TextInputStyle.Paragraph)
+			.setMaxLength(2000)
+			.setPlaceholder('Tell us everything !')
+			.setRequired(true);
+
+
+		const selectMenu = new StringSelectMenuBuilder()
+			.setCustomId('selectFeedback')
+			.setPlaceholder('Type')
+			.addOptions(
+				{
+					label: 'Bug',
+					description: 'To send a bug report to the Team',
+					value: 'bug',
+				},
+				{
+					label: 'feedback',
+					description: 'To send a feedback to the Team',
+					value: 'feedback',
+				},
+			);
 
 		// An action row only holds one text input,
 		// so you need one action row per text input.
-		const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-		const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+		const firstActionRow = new ActionRowBuilder().addComponents(typeFeedbackInput);
+		const secondActionRow = new ActionRowBuilder().addComponents(contentFeedbackInput);
+		const thirdActionRow = new ActionRowBuilder().addComponents(selectMenu);
 
 		// Add inputs to the modal
-		modal.addComponents(firstActionRow, secondActionRow);
+		modal.addComponents(firstActionRow, secondActionRow/*, thirdActionRow*/);
 
 		// Show the modal to the user
 		await interaction.showModal(modal);
-		await interaction.reply('Work In Progress');
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,23 +1,41 @@
 const { Events } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+		if (interaction.isChatInputCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
+			try {
+				await command.execute(interaction);
+			}
+			catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
 		}
-
-		try {
-			await command.execute(interaction);
-		}
-		catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
+		else if (interaction.isModalSubmit()) {
+			if (interaction.customId === 'modalFeedback') {
+				try {
+					const emote = ':speech_left:';
+					const typeFeedbackInput = interaction.fields.getTextInputValue('typeFeedbackInput');
+					const contentFeedbackInput = interaction.fields.getTextInputValue('contentFeedbackInput');
+					const feedback = `**${emote} ${typeFeedbackInput} ${emote} :** \n${contentFeedbackInput}`;
+					await interaction.client.channels.cache.get(process.env.LOG_CHANNEL_ID).send(feedback).catch(e => console.error(e));
+					await interaction.reply({ content: 'Thank you, your submission was received successfully!', ephemeral: true });
+				}
+				catch (error) {
+					console.error('Error executing feedbackModal');
+					console.error(error);
+				}
+			}
 		}
 	},
 };
