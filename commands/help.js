84d4da0b8e9d166800965c91e12b15689924de8d
AmauryVdt <amaury.vdt@gmail.com>
2023-03-09 15:09:39 +0100

    :sparkles: End the first version of Help command

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const icon = new AttachmentBuilder('./assets/WT_Esports_Standard.png');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,9 +7,19 @@ module.exports = {
 		.setDescription('Show commands and their descriptions'),
 	async execute(interaction) {
 		const embedMessage = new EmbedBuilder()
-			.setTitle('Help')
-			.setDescription('All available commands :')
-			.setColor(0xffd102);
-		await interaction.reply({ embeds: [embedMessage] });
+			.setColor(0xffd102)
+			.setTitle('TSS Bot Help Menu')
+			.setDescription('TSS Bot is a bot to get last TSS tournament for the War Thunder game.\n')
+			.setThumbnail('attachment://WT_Esports_Standard.png')
+			.addFields(
+				{ name: 'ESPORT', value: ' ' },
+				{ name: '/tournament:', value: '\nSet up channel to receive TSS tournament.' },
+				{ name: '/feedback:', value: '\nForm to send some feedback or bug report.' },
+				{ name: 'INFO', value: ' ' },
+				{ name: '/ping:', value: '\nGet the current ping of the server and the API.' },
+				{ name: '/user:', value: '\nGet info about the author of the command.' },
+				{ name: '/server:', value: '\nGet info about the current server.' },
+			);
+		await interaction.reply({ embeds: [embedMessage], files: [icon], ephemeral: true });
 	},
 };
