02f9b8b4dc41b2a58dc0bb266a0e26bb05301810
AmauryVdt <amaury.vdt@gmail.com>
2023-03-10 09:56:51 +0100

    :bug: Change the name of the game, add a space between 2 words.

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = {
 	async execute(client) {
 		if (!process.env.APP_ENV) {	console.log('APP_ENV not set yet, please set up the variable');	}
 		log.sendLog(client, log.level.SUCCESS, `Ready! Logged in as ${client.user.tag} in ${process.env.APP_ENV}`, 'Connexion').catch(e => console.error(e.message));
-		client.user.setPresence({ activities: [{ name: process.env.APP_ENV === 'PRODUCTION' ? 'WarThunder' : 'Coding the bot' }], status: 'online' });
+		client.user.setPresence({ activities: [{ name: process.env.APP_ENV === 'PRODUCTION' ? 'War Thunder' : 'Coding the bot' }], status: 'online' });
 		log.sendLog(client, log.level.INFO, `Activity set to "${client.user.presence.activities[0].name}"`, 'Presence').catch(e => console.error(e.message));
 		await tournament.tournament(client).catch(err => console.error(err));
 	},
